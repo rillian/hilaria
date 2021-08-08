163115b1ea447e31f1f319c553609fee09e6f482
Ralph Giles <giles@thaumas.net>
2021-08-08 10:00:06 -0700

    Move line annotation into a class.
    Further structure the code so each line parsed out of the csv
    export is represented by an object with associated properties
    like the page and line number reference and any notes.

--- prepare_csv.py
+++ prepare_csv.py
@@ -30,14 +30,27 @@ class LineRef:
         return cls(page, line)
 
 
+class Line:
+    '''Represent a line of coptic text with associated reference and notes.'''
+    def __init__(self, ref, coptic, note):
+        self.ref = ref
+        self.coptic = coptic
+        self.note = note
+
+    def __str__(self):
+        return f'{str(self.ref): ^5} : {self.coptic: <62} : {self.note}'
+
+
 def read_text(infilename):
     text = []
     with open(infilename, newline='') as infile:
         ref = None
         longest = 0
+
         reader = csv.reader(infile)
         # first row has the column headings
         header = reader.__next__()
+
         for row in reader:
             coptic = row[2]
             note = row[7]
@@ -47,9 +60,13 @@ def read_text(infilename):
             except ValueError:
                 # Otherwise calculate the next expected line number.
                 ref.increment()
-            print(f'{str(ref): ^5} : {coptic: <62} : {note}')
-            text.append([ref, coptic, note])
+
             longest = max(longest, len(coptic))
+
+            line = Line(ref, coptic, note)
+            text.append(line)
+            print(line)
+
     print(f'Longest Coptic line is {longest}')
     return text
 
@@ -59,7 +76,7 @@ def analyse_chars(text):
 
     chars = set()
     for line in text:
-        chars.update(set(line[1]))
+        chars.update(set(line.coptic))
 
     print('Characters in the text:')
     keys = list(chars)
